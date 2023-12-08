0cc42f77ef3fa2fe3250cb56f83ab8f4bd03dc86
David Hack <davidhack11@gmail.com>
2023-12-08 14:54:17 +1100

    Added comments to mware

--- middleware/auth.global.js
+++ middleware/auth.global.js
@@ -1,9 +1,12 @@
+// restrict to logged in users.. privacy- security
+
 export default defineNuxtRouteMiddleware((to, from) => {
    if (to.path.includes('profile')) {
       const user = useSupabaseUser()
       if (user.value) {
+         // if user contains a value, return
          return
       }
-      return navigateTo('/login')
+      return navigateTo('/login') // else require login
    }
 })
