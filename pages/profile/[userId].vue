b689e4cad4c4bc4e2b2faa177ad5e3df0c87bae1
David Hack <davidhack11@gmail.com>
2023-12-08 14:58:10 +1100

    Listings for a specific user - update for username

---
+++ pages/profile/[userId].vue
@@ -0,0 +1,29 @@
+<script setup>
+   // User listings - public page
+   definePageMeta({
+      layout: 'custom',
+   })
+
+   const route = useRoute()
+   const {userId} = route.params
+
+   const {data: listings} = await useFetch(
+      `/api/car/listings/user/${userId}`
+   )
+</script>
+
+<template>
+   {{ userId }}
+
+   <template>
+      <div>
+         <div class="shadow rounded p-3 mt-5">
+            <CarListingCard
+               v-for="listing in listings"
+               :key="listing.id"
+               :listing="listing"
+            />
+         </div>
+      </div>
+   </template>
+</template>
