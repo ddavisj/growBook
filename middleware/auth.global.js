77f7bee9e80e852685b08b53a6af4e3dabaa91db
David Hack <davidhack11@gmail.com>
2024-01-14 13:16:58 +1100

    Reconfigged mware

--- middleware/auth.global.js
+++ middleware/auth.global.js
@@ -1,12 +1,11 @@
 export default defineNuxtRouteMiddleware((to, from) => {
-   const AuthStore = useAuthStore()
-
-   if (to.path.includes('profile')) {
-      if (AuthStore.loggedIn) {
-         return
-      }
-      return navigateTo('/login')
-   }
+   // const AuthStore = useAuthStore()
+   // if (to.path.includes('profile')) {
+   //    if (AuthStore.loggedIn) {
+   //       return
+   //    }
+   //    return navigateTo('/login')
+   // }
 })
 
 // const user = useSupabaseUser()
