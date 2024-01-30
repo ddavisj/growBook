8953941d8c9ec44142d7ae38ea6cde6835818a7e
David Hack <davidhack11@gmail.com>
2024-01-30 14:01:39 +1100

    Updated check login mware

--- middleware/checkFirstLogin.js
+++ middleware/checkFirstLogin.js
@@ -1,12 +1,6 @@
 export default defineNuxtRouteMiddleware((to, from) => {
    const AuthStore = useAuthStore()
 
-   AuthStore.$subscribe(e => {
-      const username = e.events.username
-      console.log('username:', e.events.username)
-      !username ? navigateTo('/user/register') : ''
-   })
-
    // Redirect to regn on 1st login
    if (AuthStore.loggedIn && !AuthStore.username) {
       console.log('Redirected!')
